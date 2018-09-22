ec8f915dac873899d2e8befbed1a87e0f272b3d6
Lisa Felder <feldmuesle@yahoo.com>
2018-09-22 15:48:54 +0200

    make pagination a little bit more readable

--- src/DataTable/Pagination.jsx
+++ src/DataTable/Pagination.jsx
@@ -3,16 +3,14 @@ import React from 'react'
 import Page from './Page'
 
 const Pagination = ({ currentPageNumber, totalNumberOfPages, onChange }) => {
-  const pages =
-    Array
-      .from(Array(totalNumberOfPages).keys())
-      .map(pageNumber => {
-        return <Page
-          key={pageNumber}
-          currentPageNumber={currentPageNumber}
-          pageNumber={pageNumber}
-          onChange={onChange} />
-      })
+  const pageNumbers =  Array.from(Array(totalNumberOfPages).keys())
+  const pages = pageNumbers.map((pageNumber) => {
+    return <Page
+      key={pageNumber}
+      currentPageNumber={currentPageNumber}
+      pageNumber={pageNumber}
+      onChange={onChange} />
+  })
 
   if (pages.length <= 1) {
     return null
